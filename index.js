e45c5a77d278bab077bcec9d31955964d3e3a620
Eckra Wangui <+>
2023-10-04 14:22:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    const configurationObject = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch('http://localhost:3000/users', configurationObject)
+      .then(response => {
+        if (!response.ok) {
+          alert('Unauthorized Access');
+        }
+        return response.json();
+      })
+      .then(data => {
+        const id = data.id;
+        const idElement = document.createElement('p');
+        idElement.textContent = id;
+        document.body.appendChild(idElement);
+      })
+      .catch(error => {
+        const errorElement = document.createElement('p');
+        errorElement.textContent = error.message;
+        document.body.appendChild(errorElement);
+      });
+  }
+  
